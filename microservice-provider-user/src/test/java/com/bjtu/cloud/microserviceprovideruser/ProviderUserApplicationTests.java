ea6cfbeb95c3e71e1a38d969d7d941ffeec3847f
duxue <ab051448duxue@ab-insurance.com>
2018-09-19 17:09:33 +0800

    更换spark版本

--- microservice-provider-user/src/test/java/com/bjtu/cloud/microserviceprovideruser/ProviderUserApplicationTests.java
+++ microservice-provider-user/src/test/java/com/bjtu/cloud/microserviceprovideruser/ProviderUserApplicationTests.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.apache.spark.sql.SparkSession;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,17 +19,17 @@ public class ProviderUserApplicationTests {
 		System.setProperty("hadoop.home.dir",
                 "C:\\Users\\pipi\\Downloads\\hadoop-common-2.2.0-bin-master\\hadoop-common-2.2.0-bin-master");
 
-		/*SparkSession spark = SparkSession.builder()
+		SparkSession spark = SparkSession.builder()
                 .appName("Java Decition Tree Example")
                 .master("local")
                 .getOrCreate();
         Dataset<Row> data = spark.read().format("libsvm")
                 .load("./mllib/sample_libsvm_data.txt");
-        VectorIndexerModel featureIndexer = new VectorIndexer()
+       VectorIndexerModel featureIndexer = new VectorIndexer()
                 .setInputCol("features")
                 .setOutputCol("indexedFeatures")
                 .setMaxCategories(4)
-                .fit(data);*/
+                .fit(data);
 
 	}
 
