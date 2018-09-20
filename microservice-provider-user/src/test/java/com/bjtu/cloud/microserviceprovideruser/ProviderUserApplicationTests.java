1c46c0585a933ed46c930c4fdf0edfa392c690fb
duxue <ab051448duxue@ab-insurance.com>
2018-09-20 13:47:03 +0800

    修改运行spark程序报错问题

--- microservice-provider-user/src/test/java/com/bjtu/cloud/microserviceprovideruser/ProviderUserApplicationTests.java
+++ microservice-provider-user/src/test/java/com/bjtu/cloud/microserviceprovideruser/ProviderUserApplicationTests.java
@@ -1,10 +1,12 @@
 package com.bjtu.cloud.microserviceprovideruser;
 
+import org.apache.spark.ml.evaluation.RegressionEvaluator;
 import org.apache.spark.ml.feature.VectorIndexer;
 import org.apache.spark.ml.feature.VectorIndexerModel;
+import org.apache.spark.ml.regression.DecisionTreeRegressionModel;
+import org.apache.spark.ml.regression.DecisionTreeRegressor;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -14,10 +16,9 @@ import org.apache.spark.sql.SparkSession;
 @SpringBootTest
 public class ProviderUserApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	public static void main(String[] args){
 		System.setProperty("hadoop.home.dir",
-                "C:\\Users\\pipi\\Downloads\\hadoop-common-2.2.0-bin-master\\hadoop-common-2.2.0-bin-master");
+                "D:\\hadoop-2.7.3");
 
 		SparkSession spark = SparkSession.builder()
                 .appName("Java Decition Tree Example")
@@ -31,6 +32,29 @@ public class ProviderUserApplicationTests {
                 .setMaxCategories(4)
                 .fit(data);
 
+       Dataset<Row>[] splits = data.randomSplit(new double[]{0.7, 0.3});
+       Dataset<Row> trainingData = splits[0];
+       Dataset<Row> testData = splits[1];
+
+       Dataset<Row> indexedTraingData = featureIndexer.transform(trainingData);
+       Dataset<Row> indexedTestingData = featureIndexer.transform(testData);
+
+       DecisionTreeRegressor dt = new DecisionTreeRegressor()
+               .setFeaturesCol("indexedFeatures");
+
+      DecisionTreeRegressionModel model =  dt.fit(indexedTraingData);
+
+       Dataset<Row> predictions = model.transform(indexedTestingData);
+
+       predictions.select("label", "features").show();
+
+       RegressionEvaluator evaluator = new RegressionEvaluator()
+               .setLabelCol("label")
+               .setPredictionCol("prediction")
+               .setMetricName("rmse");
+       double rmse = evaluator.evaluate(predictions);
+       System.out.println("Root Mean Squared Error (RMSE) on test data = " + rmse);
+       System.out.println("Learned regression tree model:\n" + model.toDebugString());
 	}
 
 }
